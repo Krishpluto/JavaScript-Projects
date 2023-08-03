43ec7faf16a6a5a220874d433e4640eb51ee9b25
Kiruthiga <mailpersonal807@gmail.com>
2023-08-03 12:58:27 +0530

    Make the cart quantity interactive

--- Amazon clone/javascript-amazon-project-main/scripts/amazon.js
+++ Amazon clone/javascript-amazon-project-main/scripts/amazon.js
@@ -129,7 +129,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             });
         }
 
-        console.log(cart);
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        // update the cart quantity 
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     });
 });
 
