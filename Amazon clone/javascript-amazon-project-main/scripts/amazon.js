125af831d3db8b004ea6da7368418d8c39afa8de
Kiruthiga <mailpersonal807@gmail.com>
2023-08-03 11:27:14 +0530

    Use products.js file

--- Amazon clone/javascript-amazon-project-main/scripts/amazon.js
+++ Amazon clone/javascript-amazon-project-main/scripts/amazon.js
@@ -1,29 +1,38 @@
-//create a array (projects) contains objects(items and details)
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
+//create a array (projects) contains objects(items and details). step 7 - instead of adding one by one link data/products.js to create objects by creating another script tag above the amazon.js in the html file
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87
+//     },
+//     priceCents: 1090
+// }, {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: {
+//         stars: 4,
+//         count: 127
+//     },
+//     priceCents: 2095
+// }, {
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4.5,
+//         count: 56
+//     },
+//     priceCents: 799
+// }, {
+//     image: 'images/products/black-2-slot-toaster.jpg',
+//     name: '2 Slot Toaster - Black',
+//     rating: {
+//         stars: 5,
+//         count: 2197
+//     },
+//     priceCents: 1899
+
+// }];
 
 //2. combine all the html to a one string and put it in one webpage
 
